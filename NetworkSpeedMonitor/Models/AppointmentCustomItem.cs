f2d8e8e401ebc6886f32bfb3364d1d8315c7071b
chetverikov <chetverikov@sminex.com>
2024-02-24 22:57:22 +0300

    code  for developing speed control widget

---
+++ NetworkSpeedMonitor/Models/AppointmentCustomItem.cs
@@ -0,0 +1,11 @@
+﻿namespace NetworkSpeedMonitor.Models
+{
+    public class AppointmentCustomItem
+    {
+        public string StringDate { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+    }
+}
